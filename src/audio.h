fe0ec0f56d2679f52bd68273b058f3c081b20972
Ian C <ianc@noddybox.co.uk>
2026-06-20 15:54:35 +0100

    Framework for audio

---
+++ src/audio.h
@@ -0,0 +1,50 @@
+/*
+
+    espec - Sinclair Spectrum emulator
+
+    Copyright (C) 2026  Ian Cowburn (ianc@noddybox.demon.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+    -------------------------------------------------------------------------
+
+    Audio handling
+
+*/
+
+#ifndef ESPEC_TAPE_H
+#define ESPEC_TAPE_H
+
+#include <stdlib.h>
+
+/* ---------------------------------------- INTERFACES
+*/
+
+/* Initialise the audio interface.  Returns FALSE on failure to open
+   audio.  All other interfaces will silently do nothing if the device
+   can't be opened.
+*/
+int		AUDIOInit(void);
+
+/* Add the passed buffer to the sound queue.  It is a signed 8-bit mono
+   sample which will be converted as appropriate by the audio interface.
+*/
+void		AUDIOQueue(char *buffer, size_t len);
+
+
+#endif
+
+
+/* END OF FILE */
